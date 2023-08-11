9c24a2efb50a1ac44287fba097f855912f82e601
Markus Cozowicz <marcozo@microsoft.com>
2023-08-11 08:57:02 +0000

    add dependency, add classifiers fix python version

--- meteostat/setup.py
+++ meteostat/setup.py
@@ -4,11 +4,12 @@ version = ""  # don't change - this is updated by .pipelines/version-patch.py
 
 setup(name='semantic-link-functions-meteostat',
       version=version,
-      python_requires='>=3.8',
+      python_requires='>=3.10',
       description='Semantic link functions for Meteostat',
       author='Microsoft',
       url='https://github.com/microsoft/semantic-link-functions',
       packages=find_packages(exclude=["tests", "tests.*"]),
-      install_requires=["meteostat"],
+      install_requires=["meteostat", "semantic-link-sempy"],
       author_email='semanticdatascience@service.microsoft.com',
+      classifiers=['Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'License :: Other/Proprietary License', 'Programming Language :: Python :: 3.10']
       )
--- phonenumbers/setup.py
+++ phonenumbers/setup.py
@@ -4,11 +4,12 @@ version = ""  # don't change - this is updated by .pipelines/version-patch.py
 
 setup(name='semantic-link-functions-phonenumbers',
       version=version,
-      python_requires='>=3.8',
+      python_requires='>=3.10',
       description='Semantic link functions for PhoneNumbers',
       author='Microsoft',
       url='https://github.com/microsoft/semantic-link-functions',
       packages=find_packages(exclude=["tests", "tests.*"]),
-      install_requires=["phonenumbers"],
+      install_requires=["phonenumbers", "semantic-link-sempy"],
       author_email='semanticdatascience@service.microsoft.com',
+      classifiers=['Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'License :: Other/Proprietary License', 'Programming Language :: Python :: 3.10']
       )
